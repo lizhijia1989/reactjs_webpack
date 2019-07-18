4575283cf80acd3e11e41fdf9ff0a2d373c19436
zhijiali <zhijiali@Ctrip.com>
2019-07-18 16:14:56 +0800

    1

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const appRoot = path.resolve('app');
 const outputPath = path.resolve('dev');
@@ -93,6 +93,7 @@ module.exports = {
     ],
   },
   plugins: [
+    // new BundleAnalyzerPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       filename: 'index.html',
